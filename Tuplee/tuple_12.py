881b675896cbc827ae6ca73755308a2339a4849a
Nikhil Patil <np49959@gmail.com>
2026-08-13 10:38:49 +0530

    Add files via upload

---
+++ Tuplee/tuple_12.py
@@ -0,0 +1,6 @@
+numbers = []
+for i in range(5):
+    num = int(input("Enter number: "))
+    numbers.append(num)
+t = tuple(numbers)
+print("Tuple:", t)
---
+++ Tuplee/tuple_14.py
@@ -0,0 +1,4 @@
+t = (10, 20, 30)
+print("Before deleting:", t)
+del t
+print("Tuple deleted")
